16db8ab16afec9ff66576083278ef129559e65ae
treshgame <74305963+treshgame@users.noreply.github.com>
2023-01-24 23:23:50 +0300

    Add files via upload

---
+++ Laba2/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace Laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CodeSource = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.text = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CodeSource
+            // 
+            this.CodeSource.Location = new System.Drawing.Point(12, 29);
+            this.CodeSource.Name = "CodeSource";
+            this.CodeSource.Size = new System.Drawing.Size(327, 379);
+            this.CodeSource.TabIndex = 0;
+            this.CodeSource.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(514, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Лексический анализ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Код";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.text,
+            this.type});
+            this.dataGridView1.Location = new System.Drawing.Point(367, 131);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(182, 314);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // text
+            // 
+            this.text.HeaderText = "Лексема";
+            this.text.Name = "text";
+            this.text.Width = 80;
+            // 
+            // type
+            // 
+            this.type.HeaderText = "Предварительный тип";
+            this.type.Name = "type";
+            this.type.Width = 60;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(410, 448);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Label3";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView2.Location = new System.Drawing.Point(555, 131);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(120, 314);
+            this.dataGridView2.TabIndex = 8;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1});
+            this.dataGridView3.Location = new System.Drawing.Point(681, 131);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(101, 314);
+            this.dataGridView3.TabIndex = 9;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Column1";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn2});
+            this.dataGridView4.Location = new System.Drawing.Point(788, 131);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(91, 314);
+            this.dataGridView4.TabIndex = 10;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Column1";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn3});
+            this.dataGridView5.Location = new System.Drawing.Point(885, 131);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.Size = new System.Drawing.Size(112, 314);
+            this.dataGridView5.TabIndex = 11;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Column1";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(564, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Ключевые слова";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(721, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "R";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(812, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "L";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(917, 115);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "I";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 494);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView5);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CodeSource);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox CodeSource;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn text;
+        private System.Windows.Forms.DataGridViewTextBoxColumn type;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridView dataGridView5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Laba2/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba1
+{
+    public partial class Form1 : Form
+    { 
+        public List<string> Chars = new List<string> { "&", "&&", "*", "$", ";", "-", "+", "/", "{", "}", "(", ")", "=", "<", ">", ",", "()"};
+        public List<string> keyWords = new List<string> { "if", "else", "int", "string", "char", "main" };
+        // Laba2 lists
+        public List<string> L = new List<string>();
+        public List<string> currentKeyWords = new List<string>();
+        public List<string> R = new List<string>();
+        public List<string> I = new List<string>();
+        
+        public Dictionary<string, string> tableDict = new Dictionary<string, string>();
+        public Dictionary<int, int> newTableDict = new Dictionary<int, int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            L = new List<string>();
+            R = new List<string>();
+            I = new List<string>();
+            this.dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView3.Rows.Clear();
+            dataGridView4.Rows.Clear();
+            dataGridView5.Rows.Clear();
+            LexicalAnalysis();
+            TokenClassification();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        
+
+        private void LexicalAnalysis()
+        {
+            this.dataGridView1.Rows.Clear();
+            string Code = CodeSource.Text;
+            string lexem = "";
+            for (int i = 0; i < Code.Length; i++)
+            {
+                if (!lexem.Equals(""))
+                {
+                    if (Convert.ToString(Code[i]).Trim().Equals(""))
+                    {
+                        addToDict(lexem);
+                        lexem = "";
+                    }
+                    else if (Chars.Contains(Convert.ToString(Code[i])))
+                    {
+                        if (Chars.Contains(Convert.ToString(lexem) + Convert.ToString(Code[i])))
+                        {
+                            lexem += Code[i];
+                        }
+                        else
+                        {
+                            addToDict(lexem);
+                            lexem = Convert.ToString(Code[i]);
+                        }
+                    }
+                    else
+                    {
+                        if (Chars.Contains(Convert.ToString(lexem[0])))
+                        {
+                            addToDict(lexem);
+                            lexem = "";
+                        }
+                        lexem += Code[i];
+                    }
+                }
+                else
+                {
+                    lexem += Code[i];
+                }
+
+                if (i == Code.Length - 1)
+                {
+                    addToDict(lexem);
+                }
+            }
+
+            foreach (KeyValuePair<string, string> dictItem in tableDict)
+            {
+                this.dataGridView1.Rows.Add(dictItem.Key, dictItem.Value);
+            }
+        }
+        private void TokenClassification()
+        {
+            this.dataGridView1.Rows.Clear();
+            this.dataGridView1.Columns[0].HeaderText = "Таблица";
+            this.dataGridView1.Columns[1].HeaderText = "Строка";
+            if (tableDict.Any())
+            {
+                foreach (KeyValuePair<string, string> dictItem in tableDict)
+                {
+                    switch (dictItem.Value)
+                    {
+                        case "R":
+                            R.Add(dictItem.Key);
+
+                            break;
+                        case "L":
+                            L.Add(dictItem.Key);
+                            break;
+                        case "I":
+                            if (keyWords.Contains(dictItem.Key))
+                                currentKeyWords.Add(dictItem.Key);
+                            else
+                                I.Add(dictItem.Key);
+                            break;
+                    }
+                }
+                foreach (KeyValuePair<string, string> dictItem in tableDict)
+                {
+                    int index;
+                    switch (dictItem.Value)
+                    {
+                        case "R":
+                            index = Chars.FindIndex(a => a == dictItem.Key);
+                            this.dataGridView1.Rows.Add(2, index + 1);
+                            break;
+                        case "L":
+                            index = L.FindIndex(a => a == dictItem.Key);
+                            this.dataGridView1.Rows.Add(3, index + 1);
+                            break;
+                        case "I":
+                            if (keyWords.Contains(dictItem.Key))
+                            {
+                                index = currentKeyWords.FindIndex(a => a == dictItem.Key);
+                                this.dataGridView1.Rows.Add(1, index + 1);
+                            }
+                            else
+                            {
+                                index = I.FindIndex(a => a == dictItem.Key);
+                                this.dataGridView1.Rows.Add(4, index + 1);
+                            }
+                            break;
+                    }
+                }
+                addToDataGrid(keyWords, this.dataGridView2, 1);
+                addToDataGrid(Chars, this.dataGridView3, 2);
+                addToDataGrid(L, this.dataGridView4, 3);
+                addToDataGrid(I, this.dataGridView5, 4);
+            }
+        }
+        public void addToDict(string lexem)
+        {
+            if (tableDict.ContainsKey(lexem))
+            {
+                return;
+            }
+            if (int.TryParse(lexem, out int numericValue))
+            {
+                tableDict.Add(lexem, "L");
+            }
+            else
+            {
+
+                if (Chars.Contains(Convert.ToString(lexem)))
+                {
+                    tableDict.Add(lexem, "R");
+                }
+                else
+                {
+                    tableDict.Add(lexem, "I");
+                }
+
+            }
+        }
+
+        public void addToDataGrid(List<string> list, DataGridView dgv, int tableNum)
+        {
+            dgv.Rows.Clear();
+            for (int i = 0; i < list.Count; i++)
+            {
+                dgv.Rows.Add(list[i]);
+                this.dataGridView1.Rows.Add(tableNum, i+1);
+                //label3.Text += $" {cellNumber} ";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
